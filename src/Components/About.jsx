647c9f37ec510a0f9fd8959fb4654758054efa5d
09jetsalit <rovesea1@gmail.com>
2024-04-07 20:14:21 +0700

    change description about me

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Graduated from Generation Thailand bootcamp, where I studied programming fundamentals focusing on the MERN stack. I developed web applications capable of being responsive and interactive with users.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -46,12 +46,11 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  `My name is Jetsalit Jirathitiphuwadon. I graduated with a Bachelor’s
+const detailOrQuote = `My name is Jetsalit Jirathitiphuwadon. I graduated with a Bachelor’s
   Degree in Industrial Engineer. I Started my career as a Industrial
   Engineer. But now I am looking for new career path I like to finding
   new experience and developing my skill for work. I always ready
-  to learning a new things.`
+  to learning a new things.`;
 
 const About = () => {
   return (
@@ -66,10 +65,12 @@ const About = () => {
           textAlign: "center",
         }}
       >
-        <h2 style={{fontWeight: 'bold'}}>About Myself</h2>
+        <h2 style={{ fontWeight: "bold" }}>About Myself</h2>
         <p className="large">{description}</p>
         <hr />
-        <div className="flex items-center"><h2 style={{fontWeight: 'bold'}}>Skills</h2></div>
+        <div className="flex items-center">
+          <h2 style={{ fontWeight: "bold" }}>Skills</h2>
+        </div>
         <ul
           style={{
             textAlign: "left",
